aff0f1d0335e24f7343f8dcec8ff0f3815a80d3c
Erminio <Erminio>
2021-08-11 05:26:46 +0000

    Create helper fucntion composer-char-counter.js

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+    let max_length = 140;
+    $('#tweet-text').keyup(function () {
+        let len = max_length - $(this).val().length;
+        if (len < 0) {
+          $('.counter').text(len).css("color","red");
+        } else {
+        $('.counter').text(len).css("color","#545149");;
+        }
+    });
+
+});
