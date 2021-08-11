a2eb2cd4d092c4cbb44af8520fefcf57f42b6520
Erminio <Erminio>
2021-08-11 21:20:05 +0000

    edited client.js to submission using JQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,62 +4,30 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense, donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Billy",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@Bil"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1628472236712
-  },
-  {
-    "user": {
-      "name": "Emily",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@Emi"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1628558636712
-  }
-
-];
-
-
-
+//$(()=>{});
 $(document).ready(function() {
 
+  const fetchTweets = function () {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      dataType: 'json',
+      success: (tweets) => {
+        console.log(tweets);
+        renderTweets(tweets);
+      },
+      error: (err) => {
+        console.err(err);
+      }
+    })
+  }
+
+
   const renderTweets = function(users) {
-    const $tweetsContainer = $('#tweets-container');
+    //users = users.reverse()
+    const $tweetsContainer = $('#tweets-container'); // Select element from the DOM by id or class (CSS) 
     for (const user of users) {
-      $tweetsContainer.append(createTweetElement(user));
+      $tweetsContainer.prepend(createTweetElement(user));     //prepend
     }
   };
   
@@ -92,12 +60,18 @@ $(document).ready(function() {
     return $newTweet;
   };
 
-  renderTweets(data);
+  fetchTweets();
 
-  // $('#submit-form').submit(function(event) {
-  //   event.preventDefault();
-  //   console.log(event);
-  // });
+  $('#submit-form').submit(function(event) {
+    // prevent the default behaviour of the submit event (data submission and page refresh)
+    event.preventDefault();
+    //this representa form - serialize convert obj in string
+    const serializeData = $(this).serialize(); 
+    $.post('/tweets', serializeData, (response) => {
+      console.log(response);
+      fetchTweets();
+    });
+  });
 
 });
 
